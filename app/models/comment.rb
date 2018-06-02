2d4241cfd2ce6df32ab0e8ae35e29d1e059c1660
Clayton Crockett <claytoncrockett@gmail.com>
2018-06-02 16:54:20 -0600

    final project

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
 class Comment < ApplicationRecord
-  belongs_to :user, optional: true, dependent: :destroy
-  belongs_to :video, dependent: :destroy
+  belongs_to :user, optional: true
+  belongs_to :video
 end
