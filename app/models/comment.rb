986392da504f701668e5b45d84e9290907c89843
Andrew Yasso <andrew.yasso@gmail.com>
2018-06-02 16:25:59 -0600

    change the thing

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,4 @@
 class Comment < ApplicationRecord
-<<<<<<< HEAD
   belongs_to :user, optional: true, dependent: :destroy
   belongs_to :video, dependent: :destroy
-=======
-  belongs_to :user, optional: true
-  belongs_to :video
-
->>>>>>> so many changes
 end
